e5736a6c676ea2e08bde0783a65aa33780807c29
FabioHNardella <102842907+FabioHNardella@users.noreply.github.com>
2025-09-02 09:24:42 -0300

    Add files via upload

---
+++ src/main/java/br/com/absolutecode/abscode/AbscodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.absolutecode.abscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/absolutecode/abscode/AbscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.absolutecode.abscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
